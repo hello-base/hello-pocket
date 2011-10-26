b061c26f750f92a80bf4169689c280974daccd40
Bryan Veloso <bryan@revyver.com>
2011-10-27 02:28:15 +0900

    Adding an API client class for Hello! Ranking and setting the necessary flags so that shit doesn't break.

---
+++ Hello! Pocket/Classes/HPHelloRankingAPIClient.h
@@ -0,0 +1,16 @@
+//
+//  HPHelloRankingAPIClient.h
+//  Hello! Pocket
+//
+//  Created by Bryan Veloso on 10/23/11.
+//  Copyright (c) 2011 Revyver, Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "AFHTTPClient.h"
+
+extern NSString * const kHPHelloRankingBaseURLString;
+
+@interface HPHelloRankingAPIClient : AFHTTPClient
+    + (HPHelloRankingAPIClient *)sharedClient;
+@end
---
+++ Hello! Pocket/Classes/HPHelloRankingAPIClient.m
@@ -0,0 +1,42 @@
+//
+//  HPHelloRankingAPIClient.m
+//  Hello! Pocket
+//
+//  Created by Bryan Veloso on 10/23/11.
+//  Copyright (c) 2011 Revyver, Inc. All rights reserved.
+//
+
+#import "HPHelloRankingAPIClient.h"
+
+#import "AFJSONRequestOperation.h"
+
+NSString * const kHPHelloRankingBaseURLString = @"http://127.0.0.1:8000/api/v1/";
+
+@implementation HPHelloRankingAPIClient
+
++ (HPHelloRankingAPIClient *)sharedClient {
+    static HPHelloRankingAPIClient *_sharedClient = nil;
+    static dispatch_once_t oncePredicate;
+    dispatch_once(&oncePredicate, ^{
+        _sharedClient = [[self alloc] initWithBaseURL:[NSURL URLWithString:kHPHelloRankingBaseURLString]];
+    });
+    
+    return _sharedClient;
+}
+
+- (id)initWithBaseURL:(NSURL *)url {
+    self = [super initWithBaseURL:url];
+    if (!self) {
+        return nil;
+    }
+    
+    [self registerHTTPOperationClass:[AFJSONRequestOperation class]];    
+	[self setDefaultHeader:@"Accept" value:@"application/json"];
+    // [self setDefaultHeader:@"X-Hello-Ranking-API-Key" value:kHPHelloRankingClientID];
+    // [self setDefaultHeader:@"X-Hello-Ranking-API-Version" value:@"1"];
+	[self setDefaultHeader:@"X-UDID" value:[[UIDevice currentDevice] uniqueIdentifier]];
+    
+    return self;
+}
+
+@end
