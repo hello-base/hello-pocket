b027da5331d8f4739a937c0cd8842610279f5dc5
Bryan Veloso <bryan@revyver.com>
2011-11-02 23:29:31 +0900

    Adding an index to the ArtistTableView via UILocalizedIndexedCollation.

--- Classes/Controllers/HPArtistTableViewController.m
+++ Classes/Controllers/HPArtistTableViewController.m
@@ -16,12 +16,13 @@
 @property (readwrite, nonatomic, strong) NSArray *artists;
 
 - (void)loadArtists;
+- (NSArray *)partitionObjects:(NSArray *)array collationStringSelector:(SEL)selector;
 @end
 
 @implementation HPArtistTableViewController
 
 @synthesize artists = _artists;
-   
+
 #pragma mark - View Lifecycle
 
 - (void)awakeFromNib
@@ -71,7 +72,7 @@
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
 {
     if ([[segue identifier] isEqualToString:@"SegueToArtistDetail"]) {
-        Artist *artist = [self.artists objectAtIndex:self.tableView.indexPathForSelectedRow.row];
+        Artist *artist = [[self.artists objectAtIndex:self.tableView.indexPathForSelectedRow.section] objectAtIndex:self.tableView.indexPathForSelectedRow.row];
         HPArtistDetailViewController *detail = [segue destinationViewController];
         [detail setArtist:artist];
         [detail setTitle:[artist name]];
@@ -83,33 +84,73 @@
 - (void)loadArtists
 {
     [SVProgressHUD showInView:[self view]];
-    
+
     NSDictionary *limit = [NSDictionary dictionaryWithObject:@"0" forKey:@"limit"];
     [Artist fetchManyWithURLString:@"/artists" parameters:limit block:^(NSArray *records) {
-        self.artists = records;
+        self.artists = [self partitionObjects:records collationStringSelector:@selector(name)];
         [self.tableView reloadData];
     }];
 }
 
+- (NSArray *)partitionObjects:(NSArray *)array collationStringSelector:(SEL)selector
+{
+    UILocalizedIndexedCollation *collation = [UILocalizedIndexedCollation currentCollation];
+    NSInteger sectionCount = [[collation sectionTitles] count];
+    NSMutableArray *unsortedSections = [NSMutableArray arrayWithCapacity:sectionCount];
+
+    for (int i = 0; i < sectionCount; i++) {
+        [unsortedSections addObject:[NSMutableArray array]];
+    }
+
+    for (id object in array) {
+        NSInteger index = [collation sectionForObject:object collationStringSelector:selector];
+        [[unsortedSections objectAtIndex:index] addObject:object];
+    }
+
+    NSMutableArray *sections = [NSMutableArray arrayWithCapacity:sectionCount];
+
+    for (NSMutableArray *section in unsortedSections) {
+        [sections addObject:[collation sortedArrayFromArray:section collationStringSelector:selector]];
+    }
+
+    return sections;
+}
+
 #pragma mark - TableView Methods
+
+- (NSArray *)sectionIndexTitlesForTableView:(UITableView *)tableView
+{
+    return [[UILocalizedIndexedCollation currentCollation] sectionIndexTitles];
+}
+
+- (NSInteger)tableView:(UITableView *)tableView sectionForSectionIndexTitle:(NSString *)title atIndex:(NSInteger)index
+{
+    return [[UILocalizedIndexedCollation currentCollation] sectionForSectionIndexTitleAtIndex:index];
+}
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
 {
-    return 1;
+    return [[[UILocalizedIndexedCollation currentCollation] sectionTitles] count];
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-    return [self.artists count];
+    return [[self.artists objectAtIndex:section] count];
+}
+
+- (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section
+{
+    BOOL showSection = [[self.artists objectAtIndex:section] count] != 0;
+    return (showSection) ? [[[UILocalizedIndexedCollation currentCollation] sectionTitles] objectAtIndex:section] : nil;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"ArtistCell"];
-    Artist *artist = [self.artists objectAtIndex:indexPath.row];
+    Artist *artist = [[self.artists objectAtIndex:indexPath.section] objectAtIndex:indexPath.row];
     cell.textLabel.text = artist.name;
     cell.detailTextLabel.text = artist.kanji;
-    
+
     return cell;
 }
 
