04003349a502ecd5bf062957d072187c13b3cd7b
Bryan Veloso <bryan@revyver.com>
2011-11-02 23:53:13 +0900

    Anal whitespacing DictionaryHelper.

--- Vendors/iOSBoilerplate/DictionaryHelper.m
+++ Vendors/iOSBoilerplate/DictionaryHelper.m
@@ -3,7 +3,7 @@
 //  IOSBoilerplate
 //
 //  Copyright (c) 2011 Alberto Gimeno Brieba
-//  
+//
 //  Permission is hereby granted, free of charge, to any person
 //  obtaining a copy of this software and associated documentation
 //  files (the "Software"), to deal in the Software without
@@ -12,10 +12,10 @@
 //  copies of the Software, and to permit persons to whom the
 //  Software is furnished to do so, subject to the following
 //  conditions:
-//  
+//
 //  The above copyright notice and this permission notice shall be
 //  included in all copies or substantial portions of the Software.
-//  
+//
 //  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
 //  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
 //  OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
@@ -24,7 +24,7 @@
 //  WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
 //  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
 //  OTHER DEALINGS IN THE SOFTWARE.
-//  
+//
 
 #import "DictionaryHelper.h"
 
@@ -61,6 +61,5 @@
 	}
 	return s;
 }
-
 
 @end
