7bf1da1beb18d89b156778a7fd6f5aa9d0d9341e
Bryan Veloso <bryan@revyver.com>
2011-11-05 15:21:00 +0900

    Adding HPDetailTableViewController.

---
+++ Classes/Controllers/HPDetailTableViewController.h
@@ -0,0 +1,15 @@
+//
+//  HPDetailTableViewController.h
+//  Hello! Pocket
+//
+//  Created by Bryan Veloso on 11/5/11.
+//  Copyright (c) 2011 Revyver, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HPDetailTableViewController : UITableViewController
+
+@property (nonatomic, strong) id detailItem;
+
+@end
