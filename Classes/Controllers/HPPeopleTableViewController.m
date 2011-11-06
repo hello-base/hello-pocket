6add3af47817d163af5e7800b0a7b838a260befa
Bryan Veloso <bryan@revyver.com>
2011-11-06 14:03:19 +0900

    Creating a new superclass for our table views to subclass, which itself is a subclass of HPTableViewController.

---
+++ Classes/Controllers/HPPeopleTableViewController.m
@@ -0,0 +1,13 @@
+//
+//  HPPeopleTableViewController.m
+//  Hello! Pocket
+//
+//  Created by Bryan Veloso on 11/6/11.
+//  Copyright (c) 2011 Revyver, Inc. All rights reserved.
+//
+
+#import "HPPeopleTableViewController.h"
+
+@implementation HPPeopleTableViewController
+
+@end
